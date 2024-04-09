c94aa673f51884fa96646d05f1aa5d36e9c77274
PriyanshuGahlot <anshu2084gahlot@gmail.com>
2024-04-09 18:20:07 +0530

    partial working.. strict mapping of finger to mouse

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
 import cv2
+import mediapipe as mp
+import pyautogui as auto
 
 camera = cv2.VideoCapture(0)
+
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands(static_image_mode=False, max_num_hands=2, min_detection_confidence=0.2, min_tracking_confidence=0.3)
+
 
 while True:
     _,frame = camera.read()
     frame = cv2.flip(frame, 1)
+
+    results = hands.process(frame)
+
+    if results.multi_hand_landmarks:
+        for hand_landmarks in results.multi_hand_landmarks:
+            for landmark in hand_landmarks.landmark[8:9]:
+                x = int(landmark.x * frame.shape[1])
+                y = int(landmark.y * frame.shape[0])
+                print(x,y)
+                auto.moveTo(x,y)
+                cv2.circle(frame, (x, y), 5, (0, 255, 0), -1)
+
     cv2.imshow("Camera", frame)
+
     if (cv2.waitKey(1) == ord("q")):
         break
 

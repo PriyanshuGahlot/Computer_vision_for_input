578f7b1cce2a43468dd01e54a0d8f64305c87742
PriyanshuGahlot <anshu2084gahlot@gmail.com>
2024-04-09 19:13:31 +0530

    mapping of camera screen to monitor screen

--- main.py
+++ main.py
@@ -14,14 +14,24 @@ while True:
 
     results = hands.process(frame)
 
+    rectangle_width = 480
+    rectangle_height = 360
+
+
     if results.multi_hand_landmarks:
         for hand_landmarks in results.multi_hand_landmarks:
             for landmark in hand_landmarks.landmark[8:9]:
                 x = int(landmark.x * frame.shape[1])
                 y = int(landmark.y * frame.shape[0])
                 print(x,y)
-                auto.moveTo(x,y)
+                #frame is 640:480(4:3) use 600:450 then map that to the screens resolution
+                transformedX = (x/rectangle_width)*1920
+                transformedY = (y/rectangle_height)*1080
+                auto.moveTo(transformedX,transformedY)
                 cv2.circle(frame, (x, y), 5, (0, 255, 0), -1)
+
+    # Draw the rectangle on the frame
+    cv2.rectangle(frame, (0, 0), (rectangle_width, rectangle_height), (255, 0, 0), 2)
 
     cv2.imshow("Camera", frame)
 

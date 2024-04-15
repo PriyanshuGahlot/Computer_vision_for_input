71fba0c1398d5a5f7b6ee17fd1f10633e0491e66
PriyanshuGahlot <anshu2084gahlot@gmail.com>
2024-04-15 11:52:56 +0530

    data collection and processing, model training, changes in main file to perform action based on model output

--- main.py
+++ main.py
@@ -1,36 +1,64 @@
 import cv2
 import mediapipe as mp
 import pyautogui as auto
+import pickle
+import numpy as np
 
 camera = cv2.VideoCapture(0)
 
 mp_hands = mp.solutions.hands
-hands = mp_hands.Hands(static_image_mode=False, max_num_hands=2, min_detection_confidence=0.2, min_tracking_confidence=0.3)
+hands = mp_hands.Hands(static_image_mode=False,max_num_hands=1,min_tracking_confidence=0.5,min_detection_confidence=0.5)
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+
+camera = cv2.VideoCapture(0)
+
+model = pickle.load(open("model.p","rb"))["model"]
+
 
 
 while True:
-    _,frame = camera.read()
+    temp = []
+    _, frame = camera.read()
     frame = cv2.flip(frame, 1)
-
-    results = hands.process(frame)
+    result = hands.process(frame)
+    if result.multi_hand_landmarks:
+        for hand_landmarks in result.multi_hand_landmarks:
+            # mp_drawing.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS,
+            #                           mp_drawing_styles.get_default_hand_landmarks_style(),
+            #                           mp_drawing_styles.get_default_hand_connections_style())
+            for i in range(len(hand_landmarks.landmark)):
+                x = hand_landmarks.landmark[i].x
+                y = hand_landmarks.landmark[i].y
+                temp.append(x)
+                temp.append(y)
+        output = model.predict([np.asarray(temp)])
+        print(output)
+        if(output=="click"):
+            auto.mouseDown()
+        elif(output=="track"):
+            auto.mouseUp()
 
     rectangle_width = 480
     rectangle_height = 360
 
+    #print(results)
 
-    if results.multi_hand_landmarks:
-        for hand_landmarks in results.multi_hand_landmarks:
+    if result.multi_hand_landmarks:
+        for hand_landmarks in result.multi_hand_landmarks:
+            #print(hand_landmarks)
             for landmark in hand_landmarks.landmark[8:9]:
                 x = int(landmark.x * frame.shape[1])
                 y = int(landmark.y * frame.shape[0])
-                print(x,y)
+                #print(x,y)
                 #frame is 640:480(4:3) use 600:450 then map that to the screens resolution
                 transformedX = (x/rectangle_width)*1920
                 transformedY = (y/rectangle_height)*1080
                 auto.moveTo(transformedX,transformedY)
                 cv2.circle(frame, (x, y), 5, (0, 255, 0), -1)
 
-    # Draw the rectangle on the frame
+
+    #Draw the rectangle on the frame
     cv2.rectangle(frame, (0, 0), (rectangle_width, rectangle_height), (255, 0, 0), 2)
 
     cv2.imshow("Camera", frame)
